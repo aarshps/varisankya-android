c90c3f271752e790b1cfd28741e29f9439b292f2
Aarsh <aarsh@vmi3259028.contaboserver.net>
2026-04-29 14:32:25 +0200

    style: refine About page layout and update developer name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hora.varisankya"
         minSdk = 35
         targetSdk = 36
-        versionCode = 45
-        versionName = "3.8-beta.2"
+        versionCode = 47
+        versionName = "3.8-beta.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
